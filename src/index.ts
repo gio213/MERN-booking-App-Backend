fc89cb455e5511960c6c930879dcf0a0bc1d9594
Giorgi Patsia <gio.patsia@gmail.com>
2024-04-19 22:56:53 +0200

    added: user register and login flow

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from 'express';
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
-
+import userRoutes from "./routes/users"
+import authRoutes from "./routes/auth"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -14,9 +15,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({ message: "hello" })
-})
+app.use("/api/users/", userRoutes)
+app.use("/api/auth", authRoutes)
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
