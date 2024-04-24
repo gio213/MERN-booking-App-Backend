aca408d1fdc15c874be4e4b0b2de7328b4074a84
Giorgi Patsia <gio.patsia@gmail.com>
2024-04-24 17:34:08 +0200

    fixing page refresh on production

--- src/index.ts
+++ src/index.ts
@@ -42,17 +42,6 @@ app.use("/api/users/", userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/my-hotels", myHotelRoutes)
 
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
-});
-
-
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
