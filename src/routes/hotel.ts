a0df89dc784b4edb25f41bdd513d960764270849
Giorgi Patsia <gio.patsia@gmail.com>
2024-04-28 18:03:02 +0200

    added hotel detail flow

--- src/routes/hotel.ts
+++ src/routes/hotel.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import Hotel, { HotelSearchResponse } from "../models/hotel";
+import { param, validationResult } from "express-validator";
 
 const router = express.Router()
 
@@ -99,6 +100,26 @@ const constructSearchQuery = (queryParams: any) => {
 
     return constructedQuery;
 };
+
+
+router.get("/:id", [
+    param("id").notEmpty().withMessage("Hotel ID is required")
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+
+    }
+
+    const id = req.params.id.toString()
+    try {
+        const hotel = await Hotel.findById(id)
+        res.json(hotel)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching hotel" })
+    }
+})
 
 
 export default router
